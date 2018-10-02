d233d07d35b35750c3aed081f8a0639a47aae7d1
Roman Zhelieznov <rzheleznov82@outlook.com>
2018-10-03 00:30:50 +0300

    add firebase update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "max-len": ["error", {
+      "code": 200
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint',
