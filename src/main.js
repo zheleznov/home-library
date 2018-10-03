bf2ef4c8a4b6ef5dccbb6aec883d3766335f55df
Roman <roman.zhelieznov@oracle.com>
2018-10-03 16:12:09 +0300

    configure firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue';
+import firebase from 'firebase';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyC9dI6DDUB0hF1iRXhwflYvk-8KnyRc_Ak',
+  authDomain: 'home-library-e3cc8.firebaseapp.com',
+  databaseURL: 'https://home-library-e3cc8.firebaseio.com',
+  projectId: 'home-library-e3cc8',
+  storageBucket: '',
+  messagingSenderId: '255349747735',
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
