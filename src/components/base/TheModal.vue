859e21ec61b50f31a10dff4b1e77b85d6381fc9e
Roman <roman.zhelieznov@oracle.com>
2018-10-04 21:11:12 +0300

    continue working with register: create simple Modal component

---
+++ src/components/base/TheModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{title}}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>{{message}}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheModal',
+
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+    message: {
+      required: true,
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
