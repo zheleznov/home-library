fc8b4def20a2bbab5f15b5cdb43786d46114dceb
Roman Zhelieznov <rzheleznov82@outlook.com>
2018-10-05 22:30:35 +0300

    start working with register: update modal

--- src/components/base/TheModal.vue
+++ src/components/base/TheModal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
+  <div class="modal fade" tabindex="-1" role="dialog" v-if="!isDestroy">
+    <div class="modal-dialog modal-danger" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">{{title}}</h5>
@@ -12,11 +12,11 @@
           <p>{{message}}</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-primary">Save changes</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="close">Close</button>
         </div>
       </div>
     </div>
+    <div class="modal-backdrop fade show"></div>
   </div>
 </template>
 
@@ -33,10 +33,33 @@ export default {
       required: true,
       type: String,
     },
+    type: {
+      type: String,
+    },
+  },
+
+  data() {
+    return {
+      isDestroy: null,
+    };
+  },
+
+  methods: {
+    close() {
+      this.$emit('modalClose');
+    },
+  },
+
+  mounted() {
+    this.isDestroy = null;
+    this.$el.classList.add('show');
+    this.$el.style.display = 'block';
   },
 };
 </script>
 
 <style scoped>
-
+  .modal-dialog {
+    z-index: 10000;
+  }
 </style>
