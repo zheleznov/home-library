1ece68486aa59cd8519523a0d26b9a83a91d2a54
Roman Zhelieznov <rzheleznov82@outlook.com>
2018-12-02 17:48:33 +0200

    create PageNotFound view

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="clearfix">
+          <h1 class="float-left display-3 mr-4">404</h1>
+          <h4 class="pt-3">Oops! You're lost.</h4>
+          <p class="text-muted">The page you are looking for was not found.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// TODO:: maybe add link to home page
+export default {
+  name: 'PageNotFound',
+
+  beforeCreate() {
+    document.body.classList.add('flex-row', 'align-items-center');
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
