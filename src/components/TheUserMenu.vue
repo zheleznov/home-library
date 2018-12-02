c3cda92ac84b9d6605479cab46f0cabebe0c29fc
Roman Zhelieznov <rzheleznov82@outlook.com>
2018-12-02 18:35:39 +0200

    create userMenu component

---
+++ src/components/TheUserMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dropdown-menu dropdown-menu-right show">
+    <a class="dropdown-item" href="#">
+      <i class="fa fa-user"></i> Profile
+    </a>
+    <a class="dropdown-item" href="#">
+      <i class="fa fa-wrench"></i> Settings
+    </a>
+    <a class="dropdown-item" href="#" @click="logout">
+      <i class="fa fa-lock"></i> Logout
+    </a>
+    <TheSpinner v-if="isLoading"/>
+  </div>
+</template>
+
+<script>
+import TheSpinner from '../components/TheSpinner.vue';
+
+export default {
+  name: 'TheUserMenu',
+
+  components: {
+    TheSpinner,
+  },
+
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+
+  methods: {
+    logout() {
+      this.isLoading = true;
+      this.$store.dispatch('logout')
+        .then(() => {
+          // this.$router.push({ name: 'login' });
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
