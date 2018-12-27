8737d0b417dd6034a4f00111089e62cd87855d82
Roman <rzheleznov82@outlook.com>
2018-12-27 13:59:34 +0200

    userMenu component

--- src/components/TheUserMenu.vue
+++ src/components/TheUserMenu.vue
@@ -3,11 +3,11 @@
     <router-link class="dropdown-item" title="User Profile" :to="{name: 'user-profile'}">
       <i class="fa fa-user"></i> Profile
     </router-link>
-    <!--<router-link class="dropdown-item" title="User Profile" :to="{name: 'user-profile'}">
+    <!-- <router-link class="dropdown-item" title="User Profile" :to="{name: 'user-profile'}">
       <i class="fa fa-wrench"></i> Settings
-    </router-link>-->
+    </router-link> -->
     <a class="dropdown-item" href="#" @click="logout" title="Logout from Home Library">
-      <i class="fa fa-lock"></i> Logout
+      <i class="icon-logout icons"></i> Logout
     </a>
     <TheSpinner v-if="isLoading"/>
   </div>
@@ -33,7 +33,7 @@ export default {
 
   methods: {
     logout() {
-      this.$store.dispatch('logout')
+      this.$store.dispatch('users/logout')
         .then(() => {
           sessionStorage.removeItem('uid');
           this.$router.push({ name: 'login' });
