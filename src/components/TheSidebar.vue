9332a78c0f2d9d3539a9c203cc094ba6a5872bc8
Roman <rzheleznov82@outlook.com>
2018-12-27 13:59:21 +0200

    sideBar component

---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="sidebar">
+    <nav class="sidebar-nav ps ps--active-y">
+      <ul class="nav">
+        <li class="nav-title">Library</li>
+        <li class="nav-item">
+          <router-link :to="{name: 'add-book'}" class="nav-link">
+            <i class="nav-icon icon-note"></i> Add Book
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name: 'all-books'}" class="nav-link">
+            <i class="nav-icon fa fa-tasks"></i> All Books
+          </router-link>
+        </li>
+        <li class="nav-title">Labels</li>
+        <li class="nav-item d-compact-none d-minimized-none">
+          <a class="nav-label" href="#">
+            <i class="fa fa-circle text-danger"></i> Label danger</a>
+        </li>
+        <li class="nav-item d-compact-none d-minimized-none">
+          <a class="nav-label" href="#">
+            <i class="fa fa-circle text-info"></i> Label info</a>
+        </li>
+        <li class="nav-item d-compact-none d-minimized-none">
+          <a class="nav-label" href="#">
+            <i class="fa fa-circle text-warning"></i> Label warning</a>
+        </li>
+        <li class="nav-divider"></li>
+        <li class="nav-title">System Utilization</li>
+        <li class="nav-item px-3 d-compact-none d-minimized-none">
+          <div class="text-uppercase mb-1">
+            <small>
+              <b>CPU Usage</b>
+            </small>
+          </div>
+          <div class="progress progress-xs">
+            <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <small class="text-muted">348 Processes. 1/4 Cores.</small>
+        </li>
+        <li class="nav-item px-3 d-compact-none d-minimized-none">
+          <div class="text-uppercase mb-1">
+            <small>
+              <b>Memory Usage</b>
+            </small>
+          </div>
+          <div class="progress progress-xs">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <small class="text-muted">11444GB/16384MB</small>
+        </li>
+        <li class="nav-item px-3 mb-3 d-compact-none d-minimized-none">
+          <div class="text-uppercase mb-1">
+            <small>
+              <b>SSD 1 Usage</b>
+            </small>
+          </div>
+          <div class="progress progress-xs">
+            <div class="progress-bar bg-danger" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <small class="text-muted">243GB/256GB</small>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheSidebar',
+};
+</script>
+
+<style scoped>
+  .sidebar {
+    flex: 0 0 215px;
+    height: calc(100vh - 55px);
+    margin-left: -220px;
+  }
+
+  .sidebar .sidebar-nav {
+    width: 215px;
+  }
+
+  .sidebar-fixed .sidebar,
+  .sidebar .nav {
+    width: 215px;
+  }
+</style>
